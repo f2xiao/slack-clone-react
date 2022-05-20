937493867deb61dfcd279510d219f3856ef0301d
f2xiao <fxiao.mecheng@gmail.com>
2022-05-20 17:27:04 -0400

    Config firebase and router

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        header
+      </h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+function Home() {
+  return (
+    <>
+      <main>
+        <h2>Welcome to the homepage!</h2>
+        <p>You can do this, I believe in you.</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-V7Vvsc8EU1tjaU8WYdGQgoahGhU2pv4",
+  authDomain: "slack-clone-react-a6349.firebaseapp.com",
+  projectId: "slack-clone-react-a6349",
+  storageBucket: "slack-clone-react-a6349.appspot.com",
+  messagingSenderId: "466420383067",
+  appId: "1:466420383067:web:4fd2cf004b2a307f0a2e7e"
+};
